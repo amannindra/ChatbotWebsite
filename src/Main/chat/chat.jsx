220dc09982cad843053982ee6f35f9a7ace8496d
Aman Nindra <amannindra@gmail.com>
2025-01-04 12:43:27 -0800

    refactor chat and assist components; enhance styling and add delete functionality

--- src/Main/chat/chat.jsx
+++ src/Main/chat/chat.jsx
@@ -153,7 +153,6 @@ function Chat(props) {
           <img src={Photo} />
         </div>
       </div>
-
     </div>
   );
 }
--- src/assist/assist.jsx
+++ src/assist/assist.jsx
@@ -22,26 +22,20 @@ function assist(props) {
     const sidebar = document.getElementById("assisted");
     sidebar.style.width = activeTab ? "0" : "15rem";
   };
-
-
   useEffect(() => {
-    if(props.isSignedIn){
+    if (props.isSignedIn) {
       var tit = retrieveTitles();
-      tit.then(function(result){
-        if(result.length > 10){
+      tit.then(function (result) {
+        if (result.length > 10) {
           setScrollWheel(true);
-          
-        }
-        else{
+        } else {
           setScrollWheel(false);
         }
-      setTitles(result);
-    })
-    }else{
+        setTitles(result);
+      });
+    } else {
       console.log("User is not signedin");
     }
-    
-
   }, [props.currentConversation, props.isSignedIn]);
 
   const updateCurrentConv = (miniTitle) => {
@@ -49,13 +43,16 @@ function assist(props) {
   };
 
   const newConvervation = async () => {
-    if(props.isSignedIn){
-      var tit  = await addChatbot();
+    if (props.isSignedIn) {
+      var tit = await addChatbot();
       setTitles([...titles, tit]);
+    } else {
+      console.log("You need to sign in!! " + JSON.stringify(props.isSignedIn));
     }
-    else{
-      console.log("You need to sign in!! " + JSON.stringify(props.isSignedIn))
-    } 
+  };
+
+  const deleteContent = async (miniTitle) => {
+    alert("Content Deleted: " + miniTitle);
   };
 
   return (
@@ -69,20 +66,25 @@ function assist(props) {
           >
             <img src={Add} alt="Add" />
             <p>Add new Chat</p>
-            
           </div>
 
           <p id="title_text">Recent</p>
           <div className="section">
-            {titles.map((message, index) => {
-              return (
-                <div
-                  className="each_section"
-                  key={index}
-                  onClick={() => updateCurrentConv(message)}
-                >
-                  <p>{message}</p>
-                  
+    {titles.map((message, index) => {
+      return (
+        <div
+          className="each_section"
+          key={index}
+          onClick={() => updateCurrentConv(message)}
+        >
+  <p>{message}</p>
+          <a
+            className="close"
+            onClick={(e) => {
+              e.stopPropagation(); // Prevent the event from propagating to the parent
+              deleteContent(message);
+            }}
+          />
                 </div>
               );
             })}
